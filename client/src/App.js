b1a6696b14ebd484a6c106c66208122a7209373c
dresean <williamsdresean@gmail.com>
2019-03-26 12:52:59 -0400

    start register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
 import Register from './components/auth/register'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <CookiesProvider>
       <Router>
       <Route path='/register' component={Register} />
       </Router>
+      </CookiesProvider>
     );
   }
 }
